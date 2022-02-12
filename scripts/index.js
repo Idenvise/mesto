a17c5db2bd94c68b95e11f56164d7b1238010ebc
Idenvise <pavel290430@yandex.ru>
2022-02-12 16:56:42 +0700

    Проверка №2

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,33 @@
 let profileButton = document.querySelector('.profile__button');
 let popup = document.querySelector('.popup');
 let popupClose = document.querySelector('.popup__close');
-let popupInputName = document.querySelector('.popup__input');
-let popupInputSubname = document.querySelector('.popup__input:nth-of-type(2)');
+let popupInputName = document.querySelector('.popup__input_content_name');
+let popupInputSubname = document.querySelector('.popup__input_content_subname');
 let form = document.querySelector('.popup__form')
 let profileName = document.querySelector('.profile__name');
 let profileSubname = document.querySelector('.profile__subname');
-let popupSubmit = document.querySelector('.popup__save')
 
-profileButton.addEventListener('click', function () {
-  popup.classList.remove('popup_display_none');
-});
+function popupVisible () {
+  popup.classList.add('popup_visible');
+  popupInputName.value = profileName.textContent;
+  popupInputSubname.value = profileSubname.textContent;
+}
 
-popupClose.addEventListener('click', function () {
-  popup.classList.add('popup_display_none');
-});
+profileButton.addEventListener('click', popupVisible);
+
+function popupInvisible () {
+  popup.classList.remove('popup_visible');
+}
+
+popupClose.addEventListener('click', popupInvisible);
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileName.textContent = popupInputName.value;
   profileSubname.textContent = popupInputSubname.value;
-  popupInputName.setAttribute('placeholder', profileName.textContent);
-  popupInputSubname.setAttribute('placeholder', profileSubname.textContent);
-  popupInputName.value = '';
-  popupInputSubname.value = '';
-  popup.classList.add('popup_display_none');
+  popupInvisible();
 };
+
 form.addEventListener('submit', formSubmitHandler);
 
 
