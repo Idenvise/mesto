8e21af09ea61ca836c60a66062094377fa5b5c33
Idenvise <pavel290430@yandex.ru>
2022-05-08 15:50:56 +0700

    ПР 9

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,39 @@ import '../images/header__logo.svg'
 import '../images/Avatar.jpg'
 import '../pages/index.css';
 import { popupProfile, profileOpenButton, popupAdd, addButton, profileName, profileSubname, popupZoom,
-  template, sectionElements, formValidators, data,formsArr, initialCards, inputName, inputSubname } from '../utils/constants.js'
+  template, sectionElements, formValidators, data,formsArr, /*initialCards,*/ inputName, inputSubname } from '../utils/constants.js'
 import Card from '../components/Card.js'
 import FormValidation from '../components/FormValidation.js'
 import Section from '../components/Section.js'
 import PopupWithForm from '../components/PopupWithForm.js';
 import PopupWithImage from '../components/PopupWithImage.js';
 import UserInfo from '../components/UserInfo.js';
+
+function abc() {
+return fetch('https://mesto.nomoreparties.co/v1/cohort-40/cards', {
+  method: 'GET',
+  headers: {
+    authorization: '25506122-31ea-41ea-9643-f48e75424308'
+  }
+})
+.then(res => {if (res.ok) {const initialCards = res.json();
+                           return initialCards}
+else {
+  console.log('Что-то не так с начальными карточками')
+}})
+}
+fetch('https://nomoreparties.co/v1/cohort-40/users/me',
+{method: 'GET',
+  headers: {
+  authorization: '25506122-31ea-41ea-9643-f48e75424308'
+ }
+})
+.then(res => {if (res.ok){
+  return res.json();
+} else {
+  console.log('Всё идет не по плану(Профиль)')
+}}).then(res => {profileName.textContent = res.name;
+                 profileSubname.textContent = res.about});
 
 function openProfileEditor() {
   profileOpenButton.addEventListener('click', function () {
@@ -49,7 +75,7 @@ const popupAddCard = new PopupWithForm(popupAdd, addSubmitHandler);
 popupAddCard.setEventListeners();
 
 const cardList = new Section({
-  items: initialCards,
+  items: abc().then(),
   renderer: (item) => {
   cardList.addItem(createCard(item));
   }}, sectionElements);
